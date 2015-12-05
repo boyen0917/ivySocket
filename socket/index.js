8dfd10f1c87feb255f6c7b3b2525610dadcbb14f
Brian.Chen <boyen@mitake.com.tw>
2015-12-05 17:41:20 +0800

    port 3000

--- socket/index.js
+++ socket/index.js
@@ -5,7 +5,7 @@ var express = require('express'),
     io = require('socket.io')(http),
     path = require('path');
 
-var port = 8080;
+var port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
